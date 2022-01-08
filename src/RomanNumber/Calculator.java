c0dc65bb337d3a55ddb40417ed28dfd3e805ca37
Раиль <97154123+Abdulgazin@users.noreply.github.com>
2022-01-09 01:01:35 +0500

    change 09.01

--- src/RomanNumber/Calculator.java
+++ src/RomanNumber/Calculator.java
@@ -28,77 +28,84 @@ import java.util.Scanner;
     //}
 //}
     public class Calculator {
-        static Scanner scanner = new Scanner(System.in);
-        private static Object String;
+    static Scanner scanner = new Scanner(System.in);
+    private static Object String;
 
-        public static void main(String[] args) {
-            //String number1 = "1";
-            //String number2 = "1";
-            int num1 = Integer.parseInt(getLine());
-            int num2 = Integer.parseInt(getLine());
-            //int num1 = toArabic();
-            //int num2 = toArabic();
-            char operation = getOperation();
-            int result = calc(num1, num2, operation);
-            System.out.println("Результат операции: " + result);
+    public static void main(String[] args) {
+        int num1 = Integer.parseInt(RomanToArabic.getLine());
+        int num2 = Integer.parseInt(RomanToArabic.getLine());
+        char operation = RomanToArabic.getOperation();
+        int result = RomanToArabic.calc(num1, num2, operation);
+        System.out.println("Результат операции: " + result);
+    }
+
+    public static boolean isNumeric(String strNum) {
+        try {
+            int d = Integer.parseInt(strNum);
+            System.out.println("это число");
+        } catch (NumberFormatException nfe) {
+            System.out.println("это не число");
+            return false;
         }
-
-        public static boolean isNumeric(String strNum) {
-            try {
-                int d = Integer.parseInt(strNum);
-            } catch (NumberFormatException nfe) {
-                return false;
-            }
-            return true;
+        return true;
+   }
+    public static int parseInt(String strNum) throws NumberFormatException {
+        if(isNumeric(strNum)) {
+            System.out.println("String is numeric!");
+            //return parseInt(strNum);
+        } else {
+            System.out.println("String is not numeric.");
         }
-
+        return parseInt(strNum);
+    }
 //int num1 = toArabic(value1);
 //int num2 = toArabic(value2);
 
-        public static int toArabic(String value) {
-            if (value.equals("I")) return 1;
-            if (value.equals("II")) return 2;
-            if (value.equals("III")) return 3;
-            if (value.equals("IV")) return 4;
-            if (value.equals("V")) return 5;
-            if (value.equals("VI")) return 6;
-            if (value.equals("VII")) return 7;
-            if (value.equals("VIII")) return 8;
-            if (value.equals("IX")) return 9;
-            if (value.equals("X")) return 10;
-            return 0;
+    //        public static int toArabic(String value) {
+    //          if (value.equals("I")) return 1;
+//            if (value.equals("II")) return 2;
+    //          if (value.equals("III")) return 3;
+//            if (value.equals("IV")) return 4;
+    //          if (value.equals("V")) return 5;
+//            if (value.equals("VI")) return 6;
+    //          if (value.equals("VII")) return 7;
+//            if (value.equals("VIII")) return 8;
+    //          if (value.equals("IX")) return 9;
+//            if (value.equals("X")) return 10;
+    //      return 0;
+    //    }
+    private class RomanToArabic {
+        static int value(String a) {
+            if (a == "M") return 1000;
+            else if (a == "D") return 500;
+            else if (a == "C") return 100;
+            else if (a == "L") return 50;
+            else if (a == "X") return 10;
+            else if (a == "IX") return 9;
+            else if (a == "VIII") return 8;
+            else if (a == "VII") return 7;
+            else if (a == "VI") return 6;
+            else if (a == "V") return 5;
+            else if (a == "IV") return 4;
+            else if (a == "III") return 3;
+            else if (a == "II") return 9;
+            else if (a == "I") return 1;
+            else return 0;
         }
-    //private class RomanToArabic {
-          //  static int value(char a) {
-            //    if (a=='M') return 1000;
-              //  else if (a=='D') return 500;
-                //else if (a=='C') return 100;
-                //else if (a=='L') return 50;
-                //else if (a=='X') return 10;
-                //else if (a=='IX')return 9;
-                //else if (a=='VIII')return 8;
-                //else if (a=='VII')return 7;
-                //else if (a=='VI')return 6;
-                //else if (a=='V') return 5;
-                //else if (a=='IV')return 4;
-                //else if (a=='III')return 3;
-                //else if (a=='II')return 9;
-                //else if (a=='I') return 1;
-                //else return 0;
-            //}
+
         private static java.lang.String getLine() {
             System.out.print("Введите число:");
             java.lang.String num;
             if (scanner.hasNextLine()) {
                 num = scanner.nextLine();
-            }
-            else {
+            } else {
                 System.out.print("Вы допустили ошибку.");
                 scanner.next();//рекурсия
                 num = getLine();
             }
             return num;
         }
+
         public static char getOperation() {
             System.out.print("Введите операцию:");
             char operation;
@@ -111,6 +118,7 @@ import java.util.Scanner;
             }
             return operation;
         }
+
         public static int calc(int num1, int num2, char operation) {
             int result;
             switch (operation) {
@@ -133,3 +141,4 @@ import java.util.Scanner;
             return result;
         }
     }
+}
